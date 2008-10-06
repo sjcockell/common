4c1c27923ebb627cdf37d31b61206a49012b25c0
sjcockell <sjcockell@gmail.com>
2008-10-06 15:13:03 +0100

    Created SequenceHandler.py. Added README. Committer: sjcockell@gmail.com  On branch master  Changes to be committed:
    	modified:   README
    	modified:   biolibrary/SequenceHandler.py
    	modified:   biolibrary/test/SequenceHandlerTest.py

---
+++ biolibrary/test/SequenceHandlerTest.py
@@ -0,0 +1,8 @@
+import glycosylation.SequenceHandler as sh
+
+seqhandler = sh.SequenceHandler('P68133')
+id = seqhandler.get_sequence_id()
+print id
+seq = seqhandler.get_sequence()
+print seq
+seqhandler.write_sequence_file()
